7330f5ec7b9727f56dc26a982be518f9716ca31a
Deder1991 <l22619025@gmail.com>
2020-05-07 17:45:01 +0800

    增加猜的次數

--- guess_number.py
+++ guess_number.py
@@ -1,10 +1,13 @@
 import random
-
+counter = 0
 number = random.randint(1,100)
 print(number)
 while True:
-	guess_number = input("猜猜數字是多少?")
+	counter = counter + 1
+	print("這是第", counter, "次猜數字")
+	guess_number = input("猜猜數字是多少? ,")
 	guess_number = int(guess_number)
+	
 	print(guess_number)
 	if guess_number == number:
 		print("你猜對了!!")
